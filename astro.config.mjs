bed302103f1a14090a72ff0af8081366d4fd16a5
Arnav Garg <100040314+ArnavGarg10@users.noreply.github.com>
2025-07-22 12:10:44 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import vercel from '@astrojs/vercel/edge';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
+  output: "static",
   adapter: vercel()
 });
