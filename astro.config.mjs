a8dc95760e901b3d7ec498045f9931510a046b89
Arnav Garg <100040314+ArnavGarg10@users.noreply.github.com>
2025-07-22 12:11:24 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "static",
+  output: "server",
   adapter: vercel()
 });
